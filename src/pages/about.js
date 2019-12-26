8aae2d73dcfdeb2286166aa3ad882c659d57b74c
orangejuicetin <justinchoi2000@gmail.com>
2019-12-26 18:27:17 -0500

    made minor adjustments to the about.js page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutPage = () => (
+    <main>
+        <h1>About Justin Choi!</h1>
+        <p>Welcome to this work in progress! Let's hope it works out lol</p>
+        <p>This is also a way for me to present some of my projects and have some cool functionality so let's see if this works out</p>
+    </main>
+)
+
+export default AboutPage
