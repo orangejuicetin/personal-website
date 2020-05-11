3f490340969e6bc7faa941f35bbb69ec5d895dce
Justin Choi <justinchoi2000@gmail.com>
2020-05-10 23:56:13 -0400

    re-cloned onto comp, working on soon

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>This is just some test and initialization for Juice's website :DD</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
