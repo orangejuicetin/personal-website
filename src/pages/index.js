4848fee2ce187be3ca10b15293263101d2bb7356
Justin Choi <justinchoi2000@gmail.com>
2020-05-12 19:16:17 -0400

    added some more components, changed around colors to test gatsby develop

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,16 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Header from "../components/header"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Header siteTitle="Justin Choi" />
     <h1>Hi people</h1>
     <p>This is just some test and initialization for Juice's website :DD</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
