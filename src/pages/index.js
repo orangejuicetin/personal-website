eb7bcfb45c1f1f9219d000a608424666e26e4083
Justin Choi <justinchoi2000@gmail.com>
2020-05-20 22:04:06 -0400

    added more html formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,18 +9,23 @@ import Image from "../components/Image"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="image">
-      <Image />
+    <div className="top-section">
+      <div className="image">
+        <Image />
+      </div>
+      <div>
+        <article>
+          This is just some test and initialization for Juice's website :DD
+        </article>
+        <article>
+          Some more text just so that I know I can get the hang of how this is
+          supposed to be styled!
+        </article>
+        <Link to="/projects">Other works</Link>
+      </div>
     </div>
-    <div>
-      <article>
-        This is just some test and initialization for Juice's website :DD
-      </article>
-      <article>
-        Some more text just so that I know I can get the hang of how this is
-        supposed to be styled!
-      </article>
-      <Link to="/projects">Other works</Link>
+    <div className="lower-section">
+      More information here about general projects, links, etc.
     </div>
   </Layout>
 )
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,15 +4,23 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const ProjectsPage = () => (
   <Layout>
     <SEO title="Projects" />
     <div>
       <article>More stuff about projects here!</article>
-      <article>Some nice structure here</article>
+      <div>
+        <h2>Software Engineer</h2>
+        <h2>Ampe</h2>
+        <p>
+          Startup aiming to innovate the way to we plan social events, with
+          backing from the OVO fund; worked with a React frontend, as well as
+          graphQL backend.
+        </p>
+      </div>
       <Link to="/">Go back to the homepage</Link>
     </div>
   </Layout>
 )
 
-export default SecondPage
+export default ProjectsPage
